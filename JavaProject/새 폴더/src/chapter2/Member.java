e3f9f5f001d1302c7ede10f64edac1b34da9299d
beomsu1 <beumsu1221@gmail.com>
2023-03-23 23:49:02 +0900

    과제

---
+++ JavaProject/새 폴더/src/chapter2/Member.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class Member {
+
+	public static void main(String[] args) {
+		
+		String name = "이범수";
+		int age = 26;
+		double height = 178;
+		boolean hasBook = true;
+		
+		System.out.println("이름 : "+ name);
+		System.out.println("나이 : "+ age);
+		System.out.println("키 : "+ height);
+		System.out.println("책 보유 여부 : "+ hasBook);
+		
+	}
+
+}

ab8d81cd22a9d96adaf151c10e1785f8adab4cc1
beomsu1 <beumsu1221@gmail.com>
2023-04-10 18:20:50 +0900

    SQL 사용자 생성

---
+++ sql/sys_create_user_20230410.sql
@@ -0,0 +1,12 @@
+-- 2023.04.10
+-- 주석
+
+
+-- 사용자 생성
+
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+
+-- 1. 다른사용자 -> 팝업메뉴 -> 사용자 생성
+-- 2. 계정이름 C##himedia -> C## 생략하려면
+-- ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;을 실행 시켜줘야함 -> C## 생략하고 실행가능
+-- 3. 권한부여 : connect , resource , dba 
